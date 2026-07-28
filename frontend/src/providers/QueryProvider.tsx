9c21c4fb58d69d9ac4c8565eec4eefa07c6e2ac1
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-07-29 02:33:36 +0300

    add query provider file

---
+++ frontend/src/providers/QueryProvider.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/lib/query-client";
+
+interface QueryProviderProps {
+  children: React.ReactNode;
+}
+
+export function QueryProvider({ children }: QueryProviderProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  );
+}
+
+export default QueryProvider;
