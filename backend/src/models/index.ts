1b93dfb1a37da5d4f1c35e8820a91a8add28b5c6
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-07-28 11:33:50 +0300

    add models barrel export for service application admin and counter

---
+++ backend/src/models/index.ts
@@ -0,0 +1,13 @@
+import Service from "./Service";
+import Application from "./Application";
+import Admin from "./Admin";
+import Counter from "./Counter";
+
+export { Service, Application, Admin, Counter };
+
+export default {
+  Service,
+  Application,
+  Admin,
+  Counter,
+};
