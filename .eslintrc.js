da078aeb2e205a942dafd4686a0d0ce940bce418
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-07-28 16:12:24 +0300

    add frontend eslint config with react refresh plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,60 +1,17 @@
 module.exports = {
   root: true,
-  env: {
-    browser: true,
-    node: true,
-    es2022: true,
-  },
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  plugins: ["@typescript-eslint", "react", "react-hooks"],
+  env: { browser: true, es2022: true },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
     "plugin:react-hooks/recommended",
-    "prettier",
   ],
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
   rules: {
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
-      {
-        argsIgnorePattern: "^_",
-        varsIgnorePattern: "^_",
-      },
-    ],
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/no-non-null-assertion": "warn",
-    "no-console": [
-      "warn",
-      {
-        allow: ["warn", "error"],
-      },
-    ],
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
   },
-  ignorePatterns: [
-    "node_modules",
-    "dist",
-    "build",
-    ".turbo",
-    "coverage",
-    "*.config.js",
-    "*.config.ts",
-  ],
 };
