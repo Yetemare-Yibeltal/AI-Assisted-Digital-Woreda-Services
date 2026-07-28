33021e906c1bdb98aae5f60db504363fad84edf3
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-07-28 11:53:33 +0300

    add async handler wrapper to catch promise rejections in routes

---
+++ backend/src/middleware/asyncHandler.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction, RequestHandler } from "express";
+
+type AsyncRequestHandler = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => Promise<void | Response>;
+
+const asyncHandler =
+  (fn: AsyncRequestHandler): RequestHandler =>
+  (req: Request, res: Response, next: NextFunction): void => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+
+export { asyncHandler, AsyncRequestHandler };
+export default asyncHandler;
