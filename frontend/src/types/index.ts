3057667c0ad7ed97c3a60dc7acf1fc308d07d60c
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-07-29 12:55:57 +0300

    add types barrel export

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,6 @@
+export * from "./service.types";
+export * from "./application.types";
+export * from "./admin.types";
+export * from "./api.types";
+export * from "./pdf.types";
+export * from "./three.types";
