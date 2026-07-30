4aae0eb7954822e2934cc1ed41091e4adbdd1392
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-07-30 21:19:03 +0300

    add application event emitters

--- backend/src/events/applicationEvents.ts
+++ backend/src/events/applicationEvents.ts
@@ -1,1 +1,25 @@
-import eventEmitter from './eventEmitter';export const APPLICATION_SUBMITTED = 'application:submitted';export const APPLICATION_STATUS_CHANGED = 'application:statusChanged';export const emitApplicationSubmitted = (data: any) => eventEmitter.emit(APPLICATION_SUBMITTED, data);export const emitApplicationStatusChanged = (data: any) => eventEmitter.emit(APPLICATION_STATUS_CHANGED, data);
+import eventEmitter from "./eventEmitter";
+
+export const APPLICATION_SUBMITTED = "application:submitted";
+export const APPLICATION_STATUS_CHANGED = "application:statusChanged";
+export const APPLICATION_ASSIGNED = "application:assigned";
+
+export const emitApplicationSubmitted = (data: {
+  applicationId: string;
+  trackingNumber: string;
+}) => {
+  eventEmitter.emit(APPLICATION_SUBMITTED, data);
+};
+
+export const emitApplicationStatusChanged = (data: {
+  applicationId: string;
+  oldStatus: string;
+  newStatus: string;
+  changedBy: string;
+}) => {
+  eventEmitter.emit(APPLICATION_STATUS_CHANGED, data);
+};
+
+export const emitApplicationAssigned = (data: { applicationId: string; assignedTo: string }) => {
+  eventEmitter.emit(APPLICATION_ASSIGNED, data);
+};
