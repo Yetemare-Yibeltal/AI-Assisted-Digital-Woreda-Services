838beb61c77231e9c07667922e1b8502e978d845
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-07-30 20:45:32 +0300

    rewrite store hooks with typed selectors

--- frontend/src/store/hooks.ts
+++ frontend/src/store/hooks.ts
@@ -1,1 +1,15 @@
-import { useDispatch, useSelector } from "react-redux";import type { TypedUseSelectorHook } from "react-redux";import type { RootState, AppDispatch } from "./store";export const useAppDispatch: () => AppDispatch = useDispatch;export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+import { useDispatch, useSelector } from "react-redux";
+import type { TypedUseSelectorHook } from "react-redux";
+import type { RootState, AppDispatch } from "./store";
+
+// Use throughout the app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+// Selector shortcuts for common state slices
+export const useAuthState = () => useAppSelector((state) => state.auth);
+export const useServicesState = () => useAppSelector((state) => state.services);
+export const useApplicationsState = () =>
+  useAppSelector((state) => state.applications);
+export const useAdminState = () => useAppSelector((state) => state.admin);
+export const useAIState = () => useAppSelector((state) => state.ai);
