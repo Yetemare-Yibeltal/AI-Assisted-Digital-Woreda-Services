b5f6b3ff55c1f2f38383d46c068f94fd34d66f14
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-07-30 21:19:19 +0300

    add notification event emitter

--- backend/src/events/notificationEvents.ts
+++ backend/src/events/notificationEvents.ts
@@ -1,1 +1,12 @@
-import eventEmitter from './eventEmitter';export const NOTIFICATION_CREATED = 'notification:created';export const emitNotificationCreated = (data: any) => eventEmitter.emit(NOTIFICATION_CREATED, data);
+import eventEmitter from "./eventEmitter";
+
+export const NOTIFICATION_CREATED = "notification:created";
+
+export const emitNotificationCreated = (data: {
+  recipient: string;
+  title: string;
+  message: string;
+  type: string;
+}) => {
+  eventEmitter.emit(NOTIFICATION_CREATED, data);
+};
